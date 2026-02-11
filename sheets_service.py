06238d5c0b3ef4e7c2acc5abc0885c78e62a23a4
Ricardo Alvarez <ricardo.alvarez@quaianalytics.com>
2026-02-11 09:24:23 -0500

    se modifico articulos

--- sheets_service.py
+++ sheets_service.py
@@ -191,7 +191,7 @@ class GoogleSheetsService:
             "RUC",
             "Fecha",
             "Total",
-            "Artículos",
+            "Articulos",
             "Raw Data"
         ]
         
