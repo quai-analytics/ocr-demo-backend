8629cebf21d7a7ed0e3dd2181fa9a7674c4989c6
Ricardo Alvarez <ricardo.alvarez@quaianalytics.com>
2026-02-11 09:50:15 -0500

    se elimina raw_data del payload a big query

--- app.py
+++ app.py
@@ -156,7 +156,7 @@ def send_invoice_to_bigquery(invoice_data):
             "fecha": invoice_data.get("fecha", ""),
             "total": invoice_data.get("total", ""),
             "articulos": json.dumps(invoice_data.get("articulos", [])),
-            "raw_data": invoice_data
+            #"raw_data": invoice_data
         }
         print("📊 Enviando datos a BigQuery:", row)
         errors = client.insert_rows_json(table_id, [row])
